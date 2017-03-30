739717d00fa993b53f31178f15b3186738f1c3d1
Valeriy Kliuk <valeriykliuk@valeriys-imac.local>
2017-03-29 22:34:12 -0400

    Initial Commit

---
+++ GLFW/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GLFW
+//
+//  Created by Valeriy Kliuk on 2017-03-29.
+//  Copyright © 2017 Valeriy Kliuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
